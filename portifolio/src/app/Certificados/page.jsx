deb60cc4ac0aa52befdddae15a7a562dc927c63b
Kayque Miqueias <kayquemiqueias17@gmail.com>
2026-08-15 16:17:53 -0300

    teste de envio

--- portifolio/src/app/Certificados/page.jsx
+++ portifolio/src/app/Certificados/page.jsx
@@ -1,5 +1,5 @@
 import { PaginaCertificados } from "@/features/certificados";
-
+// página de certificados
 export default function Page() {
   return <PaginaCertificados />;
 }
--- portifolio/src/app/Projetos/page.jsx
+++ portifolio/src/app/Projetos/page.jsx
@@ -1,5 +1,5 @@
 import { PaginaTodosProjetos } from "@/features/todos-projetos";
-
+// página de projetos
 export default function Page() {
   return <PaginaTodosProjetos />;
 }
