86182ed2cb97a98f03b161ea64046d97cd87b626
Kayque Miqueias <kayquemiqueias17@gmail.com>
2026-08-27 23:49:10 -0300

    teste

--- portifolio/src/app/layout.jsx
+++ portifolio/src/app/layout.jsx
@@ -81,8 +81,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div className="site-shell min-h-screen">
-          <FundoPontilhadoInterativo />
+        <div className=" min-h-screen">
+          {/* site-shell */}
+          {/* <FundoPontilhadoInterativo /> */}
           <div className="relative z-10">{children}</div>
         </div>
       </body>
